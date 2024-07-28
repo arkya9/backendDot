d5acb140c292f1e4a857ea76a63c567b99b5ff84
arkya9 <arkoapple@gmail.com>
2024-07-28 11:55:13 -0700

    first commit

---
+++ BackEnd/BackEnd/DAL/Entity/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.DAL.Entity;
+
+[Table("users")]
+public partial class User
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("uname")]
+    [StringLength(255)]
+    public string? Uname { get; set; }
+
+    [Column("password")]
+    public string? Password { get; set; }
+
+    [Column("rid")]
+    public int? Rid { get; set; }
+
+    [Column("created_date", TypeName = "timestamp without time zone")]
+    public DateTime? CreatedDate { get; set; }
+
+    [Column("is_active")]
+    [StringLength(1)]
+    public string? IsActive { get; set; }
+}
---
+++ BackEnd/BackEnd/Model/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd.Model
+{
+    public class UserModel
+    {
+ 
+        public int Id { get; set; }
+
+
+        public string? Uname { get; set; }
+
+   
+        public string? Password { get; set; }
+
+        public int? Rid { get; set; }
+
+
+        public DateTime? CreatedDate { get; set; }
+        public string? IsActive { get; set; }
+    }
+}
